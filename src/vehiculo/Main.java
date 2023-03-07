788d87a02afe8d858f740576e2336d3556d0f69b
jose carlos <jckraus@outlook.es>
2023-03-07 17:05:57 +0100

    Mi primer commit KRJC2223

---
+++ src/vehiculo/Main.java
@@ -0,0 +1,41 @@
+
+package vehiculo;
+
+/**
+ *
+ * @author IES Aguadulce
+ */
+public class Main {
+    
+      public static void main(String[] args) {
+        VehiculoKRJC2223 miVehiculoKRJC2223;
+        int stockActual;
+        
+        miVehiculoKRJC2223 = new VehiculoKRJC2223("Seat",18000,100);
+        operativaVehiculosKRJC2223(miVehiculoKRJC2223, 50); 
+        stockActual = miVehiculoKRJC2223.obtenerStock();
+        System.out.println("El stock actual es"+ stockActual );
+    }
+
+    private static void operativaVehiculosKRJC2223(VehiculoKRJC2223 miVehiculoKRJC2223, int cantidad) {
+        try
+        {
+            System.out.println("Venta de Vehiculos");
+            miVehiculoKRJC2223.vender(20);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al vender");
+        }
+        
+        try
+        {
+            System.out.println("Compra de Vehiculos");
+            miVehiculoKRJC2223.comprar(100);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al comprar");
+        }
+    }
+
+}
+    
